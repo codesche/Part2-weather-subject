1596e990cb2b4884a93ae79e207002be6eeb6dda
codesche <codesche@gmail.com>
2023-01-04 12:20:29 +0900

    feat : 날씨 데이터 저장 API 작성 1
    Controller, Repository, Service 추가

---
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -0,0 +1,28 @@
+package zerobase.weather.controller;
+
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import zerobase.weather.service.DiaryService;
+
+@Controller
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE)
+                    LocalDate date, @RequestBody String text) {
+
+        diaryService.createDiary(date, text);
+    }
+
+}
