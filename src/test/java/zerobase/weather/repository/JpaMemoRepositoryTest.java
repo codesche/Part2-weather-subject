92ca565d2b4a3a8fa2f5d9895d621c0845f98679
codesche <codesche@gmail.com>
2023-01-04 12:02:17 +0900

    feat : JPA방식으로 데이터 저장

---
+++ src/test/java/zerobase/weather/repository/JpaMemoRepositoryTest.java
@@ -0,0 +1,58 @@
+package zerobase.weather.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+
+@SpringBootTest
+@Transactional
+class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest () {
+        //given
+        Memo newMemo = Memo.builder()
+                            .id(10)
+                            .text("this is jpa memo")
+                            .build();
+
+        //when
+        jpaMemoRepository.save(newMemo);
+
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+
+    }
+
+
+    @DisplayName("Jpa Test")
+    @Test
+    void findByIdTest() {
+        //given
+        Memo newMemo = Memo.builder()
+                            .id(11)
+                            .text("jpa")
+                            .build();
+
+        //when
+        Memo memo = jpaMemoRepository.save(newMemo);
+        System.out.println(memo.getId());
+
+        //then
+        Optional<Memo> result = jpaMemoRepository.findById(memo.getId());
+        assertEquals(result.get().getText(), "jpa");
+
+    }
+
+}
