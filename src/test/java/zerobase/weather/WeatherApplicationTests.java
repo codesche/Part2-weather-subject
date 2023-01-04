44ebb67c9279b621e1d12d0ac3975a284d78205a
codesche <codesche@gmail.com>
2023-01-04 12:07:22 +0900

    test: 테스트 코드 추가

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -1,27 +1,28 @@
 package zerobase.weather;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class WeatherApplicationTests {
     // 테스트 코드
-    @Test
-    void sampleTest() {
-        assertThat(1, anything());
-    }
-
     @Test
     void equalTest() {
         assertThat(1, equalTo(1));
     }
 
     @Test
-    void failTest() {
-        assertThat(1, equalTo(2));
+    void nullTest() {
+        assertNull(null);
+    }
+
+    @Test
+    void trueTest() {
+        assertTrue(1 == 1);
     }
 }
