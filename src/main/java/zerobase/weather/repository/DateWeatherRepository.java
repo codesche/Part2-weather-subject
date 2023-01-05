6615cd57de05a9d24269e6b95e98bd30e2f2e52b
codesche <codesche@gmail.com>
2023-01-05 14:57:39 +0900

    feat: Spring Scheduler 추가
    매번 다이어리 쓸 때 날씨 데이터 가져오고 파싱하는 불필요한 과정 생략
    
    날마다 API에서 날씨 데이터 가져와 저장하는 과정이 쌓이면
    이전 데이터 가져올 때 유료로 제공되는 부분을 무료로 서빙할 수 있도록 수정

---
+++ src/main/java/zerobase/weather/repository/DateWeatherRepository.java
@@ -0,0 +1,12 @@
+package zerobase.weather.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import zerobase.weather.domain.DateWeather;
+
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+
+    List<DateWeather> findAllByDate(LocalDate localDate);
+
+}
